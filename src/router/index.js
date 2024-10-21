89957ea039b8b05fb34005943e3c112880714c92
MiauRos <maurirosrdz@gmail.com>
2024-10-20 18:28:15 -0600

    refactor: fix identation

--- src/router/index.js
+++ src/router/index.js
@@ -18,12 +18,10 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: () => import('../views/MainView.vue'),
-    },{
+    },
+    {
       path: '/calendar',
       name: 'calendar',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/SelectionView.vue'),
     },
   ],
