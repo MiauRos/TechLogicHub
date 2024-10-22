f8e0d360de72fefca9df3a52905ca302cd02b763
Mauricio Rosales <Mauricio.Rosales@bmw.com.mx>
2024-10-22 11:29:35 -0600

    refactor: delete unused comments

--- src/router/index.js
+++ src/router/index.js
@@ -26,16 +26,10 @@ const router = createRouter({
     },{
       path: '/createCourse',
       name: 'createCourse',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/createCourseView.vue'),
     },{
       path: '/editCourse',
       name: 'editCourse',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/editCourseView.vue'),
     },
   ],
