8592cbb93a9952bdb8629cc3add3e9b9e4d4aa77
SaulC18 <saulcervantes.candia@gmail.com>
2024-11-21 17:17:55 -0600

    feat: add db

---
+++ techlogichub.sql
@@ -0,0 +1,223 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-11-2024 a las 00:15:12
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `techlogichub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bank`
+--
+
+CREATE TABLE `bank` (
+  `id_b` int(11) NOT NULL,
+  `nom_tit` varchar(100) NOT NULL,
+  `num` varchar(100) NOT NULL,
+  `exp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `cvc` int(11) NOT NULL,
+  `fecha_i` date NOT NULL,
+  `fecha_f` date NOT NULL,
+  `id_a` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curse`
+--
+
+CREATE TABLE `curse` (
+  `id_c` int(11) NOT NULL,
+  `id_p` int(11) NOT NULL,
+  `id_a` int(11) NOT NULL,
+  `id_m` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora_i` time NOT NULL,
+  `duracion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `students`
+--
+
+CREATE TABLE `students` (
+  `id_a` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `ape_p` varchar(100) NOT NULL,
+  `ape_m` varchar(100) NOT NULL,
+  `mat` varchar(10) NOT NULL,
+  `is_premium` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `subject`
+--
+
+CREATE TABLE `subject` (
+  `id_m` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `descr` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `id_p` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `ape_p` varchar(100) NOT NULL,
+  `ape_m` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `type`
+--
+
+CREATE TABLE `type` (
+  `id_t` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`id_b`),
+  ADD KEY `id_a` (`id_a`);
+
+--
+-- Indices de la tabla `curse`
+--
+ALTER TABLE `curse`
+  ADD PRIMARY KEY (`id_c`),
+  ADD KEY `id_p` (`id_p`,`id_a`,`id_m`);
+
+--
+-- Indices de la tabla `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id_a`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indices de la tabla `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`id_m`);
+
+--
+-- Indices de la tabla `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`id_p`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indices de la tabla `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id_t`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `type` (`type`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `bank`
+--
+ALTER TABLE `bank`
+  MODIFY `id_b` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `curse`
+--
+ALTER TABLE `curse`
+  MODIFY `id_c` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `students`
+--
+ALTER TABLE `students`
+  MODIFY `id_a` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `id_m` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `type`
+--
+ALTER TABLE `type`
+  MODIFY `id_t` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
